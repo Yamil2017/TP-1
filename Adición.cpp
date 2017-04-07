63d149372ba8ebe4e1a510df27ed40d4df592f28
Yamil2017 <pechyar@hotmail.com>
2017-04-07 18:10:21 -0300

    Add files via upload

---
+++ Adición.cpp
@@ -0,0 +1,22 @@
+//============================================================================
+// Name        : Adici�n.cpp// Author      : Yamil
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Suma de dos números enteros
+//============================================================================
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int a, b, c;
+	cout << "Ingrese número entero" << endl; // Ingreso por teclado de valor a
+
+
+	cin>>a;
+	cout << "Ingrese número entero" << endl; // Ingreso por teclado de valor b
+	cin >>b;
+	c=a+b;
+	cout << "La suma de los valores ingresados es:" << c << endl; // Presenta en consola la suma de a y b.
+}
